256ed69531e3255b8734450ee559a43b7f0b5d7e
Jason L Perry <jasper@ambethia.com>
2015-06-27 18:42:25 -0400

    Updates

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,18 +2,21 @@ class Cart < ActiveRecord::Base
   # TODO: Include Payola::Sellable
 
   has_many :line_items
-  has_many :products, :through => :line_items
+  has_many :products, through: :line_items
 
-  # methods requires by Payola
-  alias_attribute :price,     :total
-  alias_attribute :permalink, :id
-  alias_attribute :name,      :permalink
+  # additional methods required by Payola
+  alias_attribute :price, :total
+  alias_attribute :name,  :id
 
   def update_total
-    update :total => line_items.sum(:subtotal)
+    update total: line_items.sum(:subtotal)
   end
 
   def size
     line_items.sum(:quantity)
+  end
+
+  def permalink
+    Rails.application.routes.url_helpers.receipt_cart_url(id)
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,7 @@ module Nanolode
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    Rails.application.routes.default_url_options[:host] = Rails.application.secrets.domain_name
   end
 end
